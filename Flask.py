3e2a513a81b60119718dbd54dcf242fce7510e06
Denis <dogpound@mail.ru>
2020-06-06 20:02:13 +0300

    Добавил url_for для генерации URL-адреса.

--- Flask.py
+++ Flask.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -7,11 +7,15 @@ menu = ["Установка", "Первое приложение", "Обратн
 @app.route("/index")
 @app.route("/")
 def index():
+    print(url_for('index'))
     return render_template("index.html", menu=menu)
 
 @app.route("/about")
 def about():
+    print(url_for('about'))
     return render_template("about.html", title="О сайте", menu=menu)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
